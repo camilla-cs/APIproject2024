4ed90c05377dd26b3d195ca263980de4a7970495
camilla-cs <15404@coderacademy.edu.au>
2024-07-13 17:47:31 +1000

    CRUD operations for post model

--- models/post.py
+++ models/post.py
@@ -1,13 +1,13 @@
 from init import db, ma
-from marshmallow import fields, validates
-from marshmallow.validate import Length, And, Regexp, OneOf
-from marshmallow.exceptions import ValidationError
+from marshmallow import fields
+from marshmallow.validate import Length, And, Regexp
+
 
 class Post(db.Model):
     __tablename__ = "posts"
 
     id = db.Column(db.Integer, primary_key=True)
-    content= db.Column(db.String)
+    content= db.Column(db.String,nullable=False)
     date = db.Column(db.Date)
     #foreign key
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
@@ -16,18 +16,20 @@ class Post(db.Model):
     user = db.relationship('User', back_populates='posts')
 
 class PostSchema(ma.Schema): 
-    user = fields.Nested('UserSchema', only=["id", "name", "email"])
+    user = fields.Nested('UserSchema', only=["id", "username", "email"])
     content = fields.String (required=True, validate=And(
         Length(min=2, error="The content of the post must be at least two characters long."),Regexp('^[A-Za-z0-9 ]+$', error="Title must have alphanumerics characters only")
         ))
+    date=fields.DateTime()
                              
 
 class Meta: 
     fields = ("id","content","date","user")
     ordered=True
 
-#to handle a single post object
+
+
 post_schema= PostSchema()
-#to handle a list of post objects 
+
 posts_schema=PostSchema(many=True)
 
